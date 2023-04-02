0cfbc0a4deac17a2e4a2f556887f8156770e0550
Shiro san <shikidy@mail.ru>
2023-04-02 10:53:22 +0700

    Add 'block_created_at_before' param to acces transaction after some block

--- TrustWallet/TransactionsHistoryMethod.py
+++ TrustWallet/TransactionsHistoryMethod.py
@@ -3,7 +3,7 @@ import httpx
 
 # Methods for get balance 
 
-def transactions_history(address: str, chain: str, asset: str, limit:int = 25):
+def transactions_history(address: str, chain: str, asset: str, limit:int = 25, block_created_at_before: int = None):
     """Get transactions history for an address
 
     Args:
@@ -11,6 +11,7 @@ def transactions_history(address: str, chain: str, asset: str, limit:int = 25):
         chain (str): Chain to get the transactions history for Ex: bitcoin, smartchain, avalanchec
         asset (str): Asset to get the transactions history for Ex: c0, c10009000, c20000714
         limit (int, optional): Limit of transactions to get. Defaults to 25.
+        block_created_at_before (int, optional): Block created at. Defaults to None. Ex: 1620000000 (unix timestamp) 
 
     Returns:
         dict: Transactions history for the address
@@ -22,6 +23,8 @@ def transactions_history(address: str, chain: str, asset: str, limit:int = 25):
         'version': '4',
         'limit': limit,
     }
+    if block_created_at_before:
+        params['block_created_at_before'] = block_created_at
 
     url = f"https://api.trustwallet.com/v2/chains/{chain}/transactions"
     response = httpx.get(url, params=params)
